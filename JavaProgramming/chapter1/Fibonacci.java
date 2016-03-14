ab0248f742f6e67eba2cb3bc1620f9b473848b6f
Zhu,QiuZhi <zhuqiuzhi91@gmail.com>
2016-03-14 21:14:12 +0800

    输出50以内的fibonacci数列

---
+++ JavaProgramming/chapter1/Fibonacci.java
@@ -0,0 +1,15 @@
+class Fibonacci {
+	/** Print out the Fibonacci sequence for values < 50 */
+	public static void main(String[] args) {
+		int lo = 1;
+		int hi = 1;
+
+		System.out.println(lo);
+		while (hi < 50) {
+			System.out.println(hi);
+			hi = lo + hi; // new hi
+			lo = hi - lo; /* new lo is (sum - old lo)
+							 that is the old hi */
+		}
+	}
+}
