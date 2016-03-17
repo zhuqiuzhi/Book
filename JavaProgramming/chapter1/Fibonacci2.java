ba8bfab59d78f35d525d4de5e282313415e310c0
Zhu,QiuZhi <zhuqiuzhi91@gmail.com>
2016-03-17 21:31:28 +0800

    Fibonacci

--- JavaProgramming/chapter1/Fibonacci2.java
+++ JavaProgramming/chapter1/Fibonacci2.java
@@ -1,12 +1,12 @@
-class Fibonacci {
-	/** Print out the Fibonacci sequence for values < 50 */
+class Fibonacci2 {
+	static final int MAX = 50;
+	/** Print out the Fibonacci sequence for values < MAX */
 	public static void main(String[] args) {
 		int lo = 1;
 		int hi = 1;
 
-		System.out.println("the Fibonacci sequence for values < 50");
 		System.out.println(lo);
-		while (hi < 50) {
+		while (hi < MAX) {
 			System.out.println(hi);
 			hi = lo + hi; // new hi
 			lo = hi - lo; /* new lo is (sum - old lo)
