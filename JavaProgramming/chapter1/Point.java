9bbfcaa1c4d59f7f9cad2349a0b2138515a0158c
Zhu,QiuZhi <zhuqiuzhi91@gmail.com>
2016-03-19 22:24:10 +0800

    class Point,MiddlePoint

---
+++ JavaProgramming/chapter1/Point.java
@@ -0,0 +1,24 @@
+class Point {
+	public double x;
+	public double y;
+}
+
+class MakePoint {
+	public static void main(String[] argv) {
+		Point lowerLeft     = new Point();
+		Point upperRight    = new Point();
+		Point middlePoint   = new Point();
+
+		lowerLeft.x = 0.0;
+		lowerLeft.y = 0.0;
+
+		upperRight.x = 1280.0;
+		upperRight.y = 1024.0;
+
+		middlePoint.x = (lowerLeft.x + upperRight.x) / 2.0; 
+		middlePoint.y = (lowerLeft.y + upperRight.y) / 2.0;
+
+		System.out.println("middlePoint.x = " + middlePoint.x);
+		System.out.println("middlePoint.y = " + middlePoint.y);
+	}
+}
